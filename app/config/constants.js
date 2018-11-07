d0aa49ee06dc1fbc352b0009829bce75bb717dcf
jedaan <jed3an@gmail.com>
2018-11-07 22:00:54 +0200

    add rect native starter app

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyANAoq5COnJuNmff3HYwZ-18miPP0wT1CU";
+export const FIREBASE_AUTH_DOMAIN = "reactnative-31e35.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://reactnative-31e35.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "reactnative-31e35";
+export const FIREBASE_STORAGE_BUCKET = "reactnative-31e35.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "1067457277535";
+
+export const FACEBOOK_APP_ID = "<YOUR_FACEBOOK_APP_ID>";
